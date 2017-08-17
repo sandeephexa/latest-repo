6607873e71f62f93455ddcafb57209d32a195c5c
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-17 12:40:38 +0530

    Update odd_even.js

--- odd_even.js
+++ odd_even.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser=require('body-parser');
 var fs=require('fs');
+var number = req.body.result.parameters['number'];
+var message ='';
 const FACEBOOK_ACCESS_TOKEN = 'EAAFCRDYGklkBAHWOAzEnmuIPdbAEVIn6OPMoe26BH0j62TZBj1ZBMxEc2Y8xF9GFj9D6ifUn2McihyNUjYYUvbhpDexZAzRlBenVHZB8Fz26PgZCZARxOzZCay4Wxp31G6CXipPZALyZBVBWCpR0v9SenyAi6Ak9ZAQXHZAgfZBK4QeDJIdtZAhfNGFlZB6ZAycr8pHvYsZD';
 const fburl='https://graph.facebook.com/v2.6/';
 const request = require('request');
@@ -14,7 +16,7 @@ var rec_id=req.body.originalRequest.data.recipient.id;
 
     if (req.body.result.action == "input.welcome")
     {
-        if (req.body.result.resolvedQuery == "Hi") 
+        if (req.body.result.resolvedQuery == "hi") 
         {
          request(
              {
@@ -26,7 +28,7 @@ var rec_id=req.body.originalRequest.data.recipient.id;
                   let bodys=JSON.parse(body);
                   return res.json(
                   {
-                        speech:"Hi, "+bodys.first_name+" "+bodys.last_name,
+                        speech:"Welcome, "+bodys.first_name+" "+bodys.last_name,
                         displayText: "Hi, "+bodys.first_name+" "+bodys.last_name,
                         source: 'agent'
                    });
@@ -37,21 +39,21 @@ var rec_id=req.body.originalRequest.data.recipient.id;
 
     }
 
-    if (req.body.result.action == "input.unknown") 
-  {
-    if(number % 2 == 0)
+    if (req.body.result.action == "getresult") 
     {
+       if(number % 2 == 0)
+       {
          message = "Given number is Even";
-    }
-    else{
+       }
+       else{
              message = "Given number is Odd";
         }
-    return res.json({
+        return res.json({
                 speech: message,
                 displayText:message,
                 source: 'agent'
-            });
-  }
+                       });
+    }
 
 });
 
